4cfe5db13eda37d6eaed31bd22532e059ab2fef7
MariGanesh08 <mariganesh425@gmail.com>
2025-07-23 17:05:54 +0530

    leetcode problem 61

---
+++ RotateList.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        if (head == null || head.next == null || k == 0)
+            return head;
+
+        int length = 0;
+        ListNode temp = head;
+
+        while (temp != null) {
+            temp = temp.next;
+            length++;
+        }
+
+        k = k % length;
+
+        for (int i = 0; i < k; i++) {
+            head = movelast(head);
+        }
+        return head;
+    }
+
+    private ListNode movelast(ListNode head) {
+        ListNode secondlast = head;
+
+        while (secondlast.next.next != null) {
+            secondlast = secondlast.next;
+        }
+        ListNode last = secondlast.next;
+        secondlast.next = null;
+        last.next = head;
+        return last;
+    }
+}
